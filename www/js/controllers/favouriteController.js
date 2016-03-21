a9505801f6319bca5d5c334746c3be2366a678a7
IzumiSy <beetle-noise@gmx.com>
2016-03-20 20:56:28 -0700

    Add history back prevention

--- www/js/controllers/favouriteController.js
+++ www/js/controllers/favouriteController.js
@@ -1,6 +1,10 @@
 (function() {
-  var favouriteController = function($scope, $stateParams) {
-    // Do something
+  var favouriteController = function($scope, $state, $ionicPlatform) {
+
+    // Prevent trigger of history back
+    $ionicPlatform.registerBackButtonAction(function() {
+      $state.transitionTo("entrance");
+    }, 100);
   };
 
   angular
--- www/js/controllers/mashupController.js
+++ www/js/controllers/mashupController.js
@@ -1,6 +1,10 @@
 (function() {
-  var mashupController = function($scope, $stateParams) {
-    // Do something
+  var mashupController = function($scope, $state, $rootScope, $ionicPlatform) {
+
+    // Prevent trigger of history back
+    $ionicPlatform.registerBackButtonAction(function() {
+      $state.transitionTo("entrance");
+    }, 100);
   };
 
   angular
