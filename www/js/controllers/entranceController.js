dcde287e269d22c58a14ffa37d416e954488471c
IzumiSy <beetle-noise@gmx.com>
2016-03-22 00:17:00 -0700

    Add howto popup into popupService

--- www/js/controllers/entranceController.js
+++ www/js/controllers/entranceController.js
@@ -3,23 +3,14 @@
     $state,
     $scope,
     $stateParams,
-    $ionicPopup
+    popupService
   ) {
-    HOW_THIS_WORK =
-      "<p>EatMash helps you to enjoy local foods for your trip. "+
-      "This app randomly mashes up local cuisine restaurants near "+
-      "your place and suggest breakfast, lunch, and dinner that fit "+
-      "you the most!";
-
     $scope.jumpToMashup = function() {
       $state.transitionTo("tab.mashup");
     };
 
     $scope.showHowtoPopup = function() {
-      $ionicPopup.alert({
-        title: "How this work",
-        template: HOW_THIS_WORK
-      });
+      popupService.howtoPopup();
     };
   };
 
--- www/js/services/popupService.js
+++ www/js/services/popupService.js
@@ -1,17 +1,32 @@
 (function() {
-  var popupService = function($ionicPopup) {
-    var geoError = function() {
-      var ERROR_TEXT =
-        "Failed to obtain geolocation data. "+
-        "Please check if the GPS is turned on";
+  var ERROR_TEXT =
+   "Failed to obtain geolocation data. "+
+   "Please check if the GPS is turned on";
+  var HOW_THIS_WORK =
+   "<p>EatMash helps you to enjoy local foods for your trip. "+
+   "This app randomly mashes up local cuisine restaurants near "+
+   "your place and suggest breakfast, lunch, and dinner that fit "+
+   "you the most!";
 
-      $ionicPopup.alert({
+  var popupService = function($ionicPopup) {
+    var howto = function() {
+      return $ionicPopup.alert({
+        title: "How this work",
+        template: HOW_THIS_WORK
+      });
+    };
+
+    var geoError = function() {
+      return $ionicPopup.alert({
         title: "Error",
         template: ERROR_TEXT
       });
     };
 
     return {
+      howtoPopup: function() {
+        return howto();
+      },
       geoErrorPopup: function() {
         return geoError();
       }
