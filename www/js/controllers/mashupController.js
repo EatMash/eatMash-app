3798434e282cc87b239b06a8387511b876f5e960
IzumiSy <beetle-noise@gmx.com>
2016-03-22 11:43:57 -0700

    Add markers

--- www/js/controllers/mashupController.js
+++ www/js/controllers/mashupController.js
@@ -55,7 +55,65 @@
 
     // Set place markers on Google Map
     var setPlaceMarkers = function(data) {
+      if (!data.length) {
+        // TODO: Alert users that app couldnt mash up any restaurants
+        return;
+      }
 
+      var posInfo = { lat: null, lng: null };
+      data.forEach(function(d) {
+        posInfo.lat = d.coordinate.latitude;
+        posInfo.lng = d.coordinate.longitude;
+
+        var image = {
+          url: d.image_url,
+          size: new google.maps.Size(50, 50)
+        };
+
+        var infoTemplate = _.template(
+          "<div id=\"info-content\">"+
+            "<p><b><%= place_name %></b></p>"+
+            "<div class=\"address\">"+
+              "<%= place_address %>"+
+            "</div>"+
+            "<div class=\"place_phone\">"+
+              "<%= place_phone %>"+
+            "</div>"+
+          "</div>"
+        );
+        var rawInfoHtml = infoTemplate({
+          place_name: d.name,
+          place_address: (function(){
+            var text = "";
+            for (i in d.display_address) {
+              text += (" " + d.display_address[i]);
+            }
+            return text;
+          })(),
+          place_phone: (function() {
+            if (d.phone) {
+              return '<p>' + d.phone + '</p>';
+            } else {
+              return null;
+            }
+          })()
+        });
+
+        var info = new google.maps.InfoWindow({
+          content: rawInfoHtml
+        });
+
+        var marker = new google.maps.Marker({
+          position: posInfo,
+          map: $scope.map,
+          icon: image,
+          title: d.name
+        });
+
+        marker.addListener('click', function() {
+          info.open($scope.map, marker);
+        });
+      });
     };
 
     $document.ready(function() {
@@ -76,11 +134,12 @@
       // Render markers
       doMashup("Soma", function(isSuccess, data) {
         $ionicLoading.hide();
-        if (isSuccess) {
+        if (!isSuccess) {
           // TODO: Alert users that app got an error in fetching API.
           return
         }
 
+        console.log(data);
         setPlaceMarkers(data);
       });
     });
